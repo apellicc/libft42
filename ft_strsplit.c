f755034e505238b154d4a4cbfe120cabbfc37ccf
Armando PELLICCIA <apellicc@e1r8p21.42.fr>
2015-12-01 21:09:40 +0100

    finish part no norme

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -11,56 +11,58 @@ int		ft_nbword(char const *s, char c)
 		i = 1;
 	while (s[a] != '\0')
 	{
-		if (s[a] == c && s[a + 1] != c)
+		if (s[a] == c && s[a + 1] != c && s[a + 1] != '\0' && s[a] !='\0')
+		{
+			a++;
 			i++;
+		}
 		a++;
 	}
-	printf("%d valeur final de i\n", i);
 	return (i);
 }
 
 
 int		ft_lenword(const char *s, char c, int i)
 {
-	while (s[i] != c && s[i + 1] != '\0')
-		i++;
-	return (i);
+	int a;
+	a = i;
+	while(s[a] != c && s[a] != '\0')
+		a++;
+	return (a - i);
 }
 char	**ft_strsplit(char const *s, char c)
 {
 	printf("\n%s string base\n", s);
-	printf("%c a supp\n", c);
+	printf("%c supp\n", c);
 	int a;
 	int b;
 	int i;
+	int lol;
 	char **str;
-
+	lol = 0;
 	a = 0;
 	i = 0;
-	str = (char **)malloc(sizeof(char *) * (ft_nbword(s, c) + 1));
+	str = (char **)malloc(sizeof(char *) * ft_nbword(s, c) + 1 );
 	if(!str)
 		return(NULL);
-	while (s[i] != '\0')
+	while (lol < ft_nbword(s, c))
 	{
-	    while(s[i] == c)
+	    while (s[i] == c && s[i] != '\0')
 			i++;
-		if (s[i] != c)
-		{
-			b = ft_lenword(s, c, i) - i ;
-			printf("%d lenword\n", b);
-			str[a] = ft_strnew(b + 1 );
-			if(!str[a])
-				return(NULL);
-			str[a] = ft_strncpy(str[a], (s + i), b);
-			i = i + b;
-			printf("%smon str[a]\n", str[a]);
-			printf("%d mot numero\n" ,a );
-			a++;
+		if (s[i] != c && s[i + 1] != '\0' && s[i] != '\0')
+		{	
+			b = ft_lenword(s, c, i);
+			printf("%d valeur de lenword\n", ft_lenword(s, c, i));
+			printf("%d nombre de mots\n", ft_nbword(s, c));
+			str[a++] = ft_strsub(s, i, b);
+			i = i + b - 1;
+			printf("%s mon str[", str[a]);
+			printf("%d]\n" ,a );
 
 		}
 		i++;
-		str[a] = NULL;
+		lol++;
 	}
-
+	str[a] = NULL;
 	return (str);
 }
