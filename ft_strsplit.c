fce0364d1f32aeb06e3e732127c2a792c8aa0683
Armando Pelliccia <apellicc@student.42.fr>
2016-03-27 17:18:46 +0200

    correction

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: apellicc <apellicc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 16:42:13 by apellicc          #+#    #+#             */
-/*   Updated: 2016/03/27 12:24:54 by apellicc         ###   ########.fr       */
+/*   Updated: 2016/03/27 17:16:14 by apellicc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ char		**ft_strsplit(char const *s, char c)
 	lol = -1;
 	a = 0;
 	i = -1;
-	str = (char **)malloc(sizeof(char *) * ft_nbword(s, c) + 1);
+	str = (char **)malloc(sizeof(char *) * (ft_nbword(s, c) + 1));
 	if (!str)
 		return (NULL);
 	while (++lol < ft_nbword(s, c) && ++i >= 0)
 	{
 		while (s[i] == c && s[i] != '\0')
 			i++;
-		if (s[i] != c && s[i + 1] != '\0' && s[i] != '\0')
+		if (s[i] != c &&s[i] != '\0' )//&& s[i + 1] == '\0' && s[i] != '\0')
 		{
 			b = ft_lenword(s, c, i);
 			str[a++] = ft_strsub(s, i, b);
